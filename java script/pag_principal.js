d385a312857cd1b41f629b62fe08a4dbca40966f
jhonatan farias de lima <63428042+jhonatanfariasdelima@users.noreply.github.com>
2021-03-03 22:29:48 -0300

    entrada dos comandos aritimeticos pelo teclado

--- java script/pag_principal.js
+++ java script/pag_principal.js
@@ -1,24 +1,28 @@
 var total = [0, 0, 0];
 operacao = '';
-$(document).ready(function(){
-
+$(document).ready(function () {
+	
 	escreve_num();
 	escreve_sinal();
 	calculo();
-	
+
+	$("#igual").click(function () {
+		calculo();
+	})
+2
 });
 
 
-document.addEventListener("keypress", function(e) {
-	if(!bloq_alfabeto(e)){
+document.addEventListener("keypress", function (e) {
+	if (!bloq_alfabeto(e)) {
 		e.preventDefault();
 
-	}else if(e.keyCode == 44){
-		if(document.getElementById('resultados').value ==  ''){
+	} else if (e.keyCode == 44) {
+		if (document.getElementById('resultados').value == '') {
 
 			document.getElementById('resultados').value = document.getElementById('resultados').value + '0';
 
-		}else if(document.getElementById('resultados').value.indexOf(',') > 0){
+		} else if (document.getElementById('resultados').value.indexOf(',') > 0) {
 
 			e.preventDefault();
 		}
@@ -26,7 +30,7 @@ document.addEventListener("keypress", function(e) {
 
 });
 
-function bloq_alfabeto(e){
+function bloq_alfabeto(e) {
 
 	const char = String.fromCharCode(e.keyCode);
 
@@ -35,61 +39,131 @@ function bloq_alfabeto(e){
 
 	const padrao = '[0-9]';
 
-	if(e.keyCode == 44){
+	if (e.keyCode == 44) { /*virgula*/ 
 		return true;
 
-	}else if(char.match(padrao)){
+	} else if (char.match(padrao)) {
 		return true;
+
+	}else if(e.keyCode == 45){
+	
+		if (document.getElementById('resultados').value == '') {
+
+			document.getElementById('resultados').value = document.getElementById('resultados').value;
+
+		} else {
+			total[0] = document.getElementById('resultados').value /*adiciona o primeiro numero a lista*/
+
+			document.getElementById('subtrair').style.backgroundColor = "cyan"; /*muda a cor do botão para 
+																	identificar a operação atual para o usuario*/
+
+			document.getElementById('resultados').value = '';
+
+			operacao = 's';  /*operação de subtracao*/
+			
+		}
+	}else if(e.keyCode == 43){
+	
+		if (document.getElementById('resultados').value == '') {
+
+			document.getElementById('resultados').value = document.getElementById('resultados').value;
+
+		} else {
+			total[0] = document.getElementById('resultados').value 
+
+			document.getElementById('adicao').style.backgroundColor = "cyan"; 
+
+			document.getElementById('resultados').value = '';
+
+			operacao = 'a';  
+			
+		}
+	}else if(e.keyCode == 47){
+	
+		if (document.getElementById('resultados').value == '') {
+
+			document.getElementById('resultados').value = document.getElementById('resultados').value;
+
+		} else {
+			total[0] = document.getElementById('resultados').value 
+
+			document.getElementById('dividir').style.backgroundColor = "cyan"; 
+
+			document.getElementById('resultados').value = '';
+
+			operacao = 'd';  
+			
+		}
+	}else if(e.keyCode == 42){
+	
+		if (document.getElementById('resultados').value == '') {
+
+			document.getElementById('resultados').value = document.getElementById('resultados').value;
+
+		} else {
+			total[0] = document.getElementById('resultados').value 
+
+			document.getElementById('multiplicar').style.backgroundColor = "cyan"; 
+
+			document.getElementById('resultados').value = '';
+
+			operacao = 'm';  
+			
+		}
+	}else if(e.keyCode == 13){
+		calculo();
+		
 	}
+
 
 }
 
 function escreve_num() {
-	$("#00").click(function(){
+	$("#00").click(function () {
 		document.getElementById('resultados').value = document.getElementById('resultados').value + '00';
 	})
 
-	$("#0").click(function(){
+	$("#0").click(function () {
 		document.getElementById('resultados').value = document.getElementById('resultados').value + '0';
 	})
 
-	$("#1").click(function(){
+	$("#1").click(function () {
 		document.getElementById('resultados').value = document.getElementById('resultados').value + '1';
 	})
 
-	$("#2").click(function(){
+	$("#2").click(function () {
 		document.getElementById('resultados').value = document.getElementById('resultados').value + '2';
 	})
 
-	$("#3").click(function(){
+	$("#3").click(function () {
 		document.getElementById('resultados').value = document.getElementById('resultados').value + '3';
 	})
 
-	$("#4").click(function(){
+	$("#4").click(function () {
 		document.getElementById('resultados').value = document.getElementById('resultados').value + '4';
 	})
 
-	$("#5").click(function(){
+	$("#5").click(function () {
 		document.getElementById('resultados').value = document.getElementById('resultados').value + '5';
 	})
 
-	$("#6").click(function(){
+	$("#6").click(function () {
 		document.getElementById('resultados').value = document.getElementById('resultados').value + '6';
 	})
 
-	$("#7").click(function(){
+	$("#7").click(function () {
 		document.getElementById('resultados').value = document.getElementById('resultados').value + '7';
 	})
 
-	$("#8").click(function(){
+	$("#8").click(function () {
 		document.getElementById('resultados').value = document.getElementById('resultados').value + '8';
 	})
 
-	$("#9").click(function(){
+	$("#9").click(function () {
 		document.getElementById('resultados').value = document.getElementById('resultados').value + '9';
 	})
 
-	$("#clear").click(function(){
+	$("#clear").click(function () {
 		document.getElementById('resultados').value = '';
 		document.getElementById('dividir').style.backgroundColor = "";
 		document.getElementById('adicao').style.backgroundColor = "";
@@ -98,124 +172,123 @@ function escreve_num() {
 
 	})
 
-	}
+}
 
 
 function escreve_sinal() {
 
-	$("#subtrair").click(function(){
-		if(document.getElementById('resultados').value ==  ''){
+	$("#subtrair").click(function () {
+		if (document.getElementById('resultados').value == '') {
 
 			document.getElementById('resultados').value = document.getElementById('resultados').value;
 
-		}else{
+		} else {
 			total[0] = document.getElementById('resultados').value /*adiciona o primeiro numero a lista*/
 
 			document.getElementById('subtrair').style.backgroundColor = "cyan"; /*muda a cor do botão para 
 																	identificar a operação atual para o usuario*/
 
 			document.getElementById('resultados').value = '';
-			
+
 			operacao = 's';  /*operação de subtracao*/
 		}
 	})
 
-	$("#adicao").click(function(){
-		if(document.getElementById('resultados').value ==  ''){
+	$("#adicao").click(function () {
+		if (document.getElementById('resultados').value == '') {
 
 			document.getElementById('resultados').value = document.getElementById('resultados').value;
 
-		}else{
+		} else {
 			total[0] = document.getElementById('resultados').value /*adiciona o primeiro numero a lista*/
 
 			document.getElementById('adicao').style.backgroundColor = "cyan"; /*muda a cor do botão para 
 																	identificar a operação atual para o usuario*/
 
 			document.getElementById('resultados').value = '';
-			
+
 			operacao = 'a';  /*operação de adição*/
 		}
 	})
 
-	$("#multiplicar").click(function(){
-		if(document.getElementById('resultados').value ==  ''){
+	$("#multiplicar").click(function () {
+		if (document.getElementById('resultados').value == '') {
 
 			document.getElementById('resultados').value = document.getElementById('resultados').value;
 
-		}else{
+		} else {
 			total[0] = document.getElementById('resultados').value /*adiciona o primeiro numero a lista*/
 
 			document.getElementById('multiplicar').style.backgroundColor = "cyan"; /*muda a cor do botão para 
 																	identificar a operação atual para o usuario*/
 
 			document.getElementById('resultados').value = '';
-			
+
 			operacao = 'm';  /*operação de multiplicação*/
 		}
 	})
 
-	$("#dividir").click(function(){
-		if(document.getElementById('resultados').value ==  ''){
+	$("#dividir").click(function () {
+		if (document.getElementById('resultados').value == '') {
 
 			document.getElementById('resultados').value = document.getElementById('resultados').value;
 
-		}else{
+		} else {
 			total[0] = document.getElementById('resultados').value /*adiciona o primeiro numero a lista*/
 
 			document.getElementById('dividir').style.backgroundColor = "cyan"; /*muda a cor do botão para 
 																	identificar a operação atual para o usuario*/
 
 			document.getElementById('resultados').value = '';
-			
+
 			operacao = 'd';  /*operação de divisão*/
 		}
 	})
 
 
-	$("#virgula").click(function(){
+	$("#virgula").click(function () {
 
-		if(document.getElementById('resultados').value ==  ''){
+		if (document.getElementById('resultados').value == '') {
 
 			document.getElementById('resultados').value = document.getElementById('resultados').value + '0,';
 
-		}else if(document.getElementById('resultados').value.indexOf(',') == -1){
+		} else if (document.getElementById('resultados').value.indexOf(',') == -1) {
 			document.getElementById('resultados').value = document.getElementById('resultados').value + ',';
-		
+
 		}
 	})
 }
 
 function calculo() {
-	$("#igual").click(function(){
-		total[1] = document.getElementById('resultados').value /*adiciona o segundo numero a lista*/
-		
-		if(operacao == 'd'){
-			total[2] = total[0] / total[1];
-			document.getElementById('resultados').value = total[2];
-			document.getElementById('dividir').style.backgroundColor = "";
-			operacao = '';
+	
+	total[1] = document.getElementById('resultados').value /*adiciona o segundo numero a lista*/
 
-		}else if(operacao == 'a'){
-			total[2] = parseInt(total[0]) + parseInt(total[1]);
-			document.getElementById('resultados').value = total[2];
-			document.getElementById('adicao').style.backgroundColor = "";
-			operacao = '';
+	if (operacao == 'd') {
+		total[2] = total[0] / total[1];
+		document.getElementById('resultados').value = total[2];
+		document.getElementById('dividir').style.backgroundColor = "";
+		operacao = '';
 
-		}else if(operacao == 's'){
-			total[2] = parseInt(total[0]) - parseInt(total[1]);
-			document.getElementById('resultados').value = total[2];
-			document.getElementById('subtrair').style.backgroundColor = "";
-			operacao = '';
+	} else if (operacao == 'a') {
+		total[2] = parseInt(total[0]) + parseInt(total[1]);
+		document.getElementById('resultados').value = total[2];
+		document.getElementById('adicao').style.backgroundColor = "";
+		operacao = '';
 
-		}else if(operacao == 'm'){
-			total[2] = parseInt(total[0]) * parseInt(total[1]);
-			document.getElementById('resultados').value = total[2];
-			document.getElementById('multiplicar').style.backgroundColor = "";
-			operacao = '';
-		}
+	} else if (operacao == 's') {
+		total[2] = parseInt(total[0]) - parseInt(total[1]);
+		document.getElementById('resultados').value = total[2];
+		document.getElementById('subtrair').style.backgroundColor = "";
+		operacao = '';
 
-		console.log(total);
-	})
+	} else if (operacao == 'm') {
+		total[2] = parseInt(total[0]) * parseInt(total[1]);
+		document.getElementById('resultados').value = total[2];
+		document.getElementById('multiplicar').style.backgroundColor = "";
+		operacao = '';
+	}
+
+	console.log(total);
 }
 
 
